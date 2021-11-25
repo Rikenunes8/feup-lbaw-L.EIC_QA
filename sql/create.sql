4c3a8ec9ce64c8527755f0b085c164be233a1c2a
Henrique Nunes <hrn2001@gmail.com>
2021-11-25 14:28:03 +0000

    Bug fix

--- sql/create.sql
+++ sql/create.sql
@@ -34,15 +34,15 @@ CREATE TABLE "utilizador" (
     pontuacao     INTEGER DEFAULT 0,
     bloqueado     BOOLEAN DEFAULT FALSE,
     ano_ingresso  INTEGER,
-    TYPE tipo_utilizador NOT NULL,
+    tipo tipo_utilizador NOT NULL,
 
-    CONSTRAINT nome_NN         CHECK ((tipo_utilizador='Administrador' AND nome IS NULL) OR (tipo_utilizador<>'Administrador' AND nome IS NOT NULL)),
-    CONSTRAINT foto_perfil_NN  CHECK ((tipo_utilizador='Administrador' AND foto_perfil IS NULL) OR (tipo_estado<>'Administrador')),
-    CONSTRAINT sobre_NN        CHECK ((tipo_utilizador='Administrador' AND sobre IS NULL) OR (tipo_estado<>'Administrador')),
-    CONSTRAINT data_nasc_NN    CHECK ((tipo_utilizador='Administrador' AND data_nascimento IS NULL) OR (tipo_estado<>'Administrador')),
-    CONSTRAINT bloqueado_NN    CHECK ((tipo_utilizador='Administrador' AND bloqueado IS NULL) OR (tipo_utilizador<>'Administrador' AND bloqueado IS NOT NULL)),
-    CONSTRAINT pontuacao_NN    CHECK ((tipo_utilizador='Administrador' AND pontuacao IS NULL) OR (tipo_utilizador<>'Administrador' AND pontuacao IS NOT NULL AND pontuacao >= 0)),
-    CONSTRAINT ano_ingresso_NN CHECK ((tipo_utilizador<>'Aluno' AND ano_ingresso IS NULL) OR (tipo_utilizador='Aluno' AND ano_ingresso IS NOT NULL AND ano_ingresso > 0))
+    CONSTRAINT nome_NN         CHECK ((tipo='Administrador' AND nome IS NULL) OR (tipo<>'Administrador' AND nome IS NOT NULL)),
+    CONSTRAINT foto_perfil_NN  CHECK ((tipo='Administrador' AND foto_perfil IS NULL) OR (tipo<>'Administrador')),
+    CONSTRAINT sobre_NN        CHECK ((tipo='Administrador' AND sobre IS NULL) OR (tipo<>'Administrador')),
+    CONSTRAINT data_nasc_NN    CHECK ((tipo='Administrador' AND data_nascimento IS NULL) OR (tipo<>'Administrador')),
+    CONSTRAINT bloqueado_NN    CHECK ((tipo='Administrador' AND bloqueado IS NULL) OR (tipo<>'Administrador' AND bloqueado IS NOT NULL)),
+    CONSTRAINT pontuacao_NN    CHECK ((tipo='Administrador' AND pontuacao IS NULL) OR (tipo<>'Administrador' AND pontuacao IS NOT NULL AND pontuacao >= 0)),
+    CONSTRAINT ano_ingresso_NN CHECK ((tipo<>'Aluno' AND ano_ingresso IS NULL) OR (tipo='Aluno' AND ano_ingresso IS NOT NULL AND ano_ingresso > 0))
 );
 
 create table "uc" (
@@ -70,14 +70,14 @@ CREATE TABLE "intervencao" (
     texto           TEXT NOT NULL,
     data            DATE NOT NULL DEFAULT now(),
     pontuacao       INTEGER NOT NULL DEFAULT 0,
-    titulo          TEXT
+    titulo          TEXT,
     categoria       INTEGER REFERENCES uc ON DELETE CASCADE ON UPDATE CASCADE,
     id_intervencao  INTEGER REFERENCES intervencao ON DELETE CASCADE ON UPDATE CASCADE,
-    TYPE tipo_intervencao NOT NULL,
+    tipo tipo_intervencao NOT NULL,
 
     CONSTRAINT data_menor_agora   CHECK (data <= now()),
-    CONSTRAINT titulo_categ_NN    CHECK ((tipo_intervencao ='questao' AND titulo IS NOT NULL AND categoria IS NOT NULL) OR (tipo_intervencao<>'questao' AND titulo IS NULL AND categoria IS NULL)),
-    CONSTRAINT id_intervencao_NN  CHECK ((tipo_intervencao<>'questao' AND id_intervencao IS NOT NULL) OR (tipo_intervencao='questao' AND id_intervencao IS NULL))
+    CONSTRAINT titulo_categ_NN    CHECK ((tipo ='questao' AND titulo IS NOT NULL AND categoria IS NOT NULL) OR (tipo<>'questao' AND titulo IS NULL AND categoria IS NULL)),
+    CONSTRAINT id_intervencao_NN  CHECK ((tipo<>'questao' AND id_intervencao IS NOT NULL) OR (tipo='questao' AND id_intervencao IS NULL))
 );
 
 CREATE TABLE "votacao" (
@@ -102,16 +102,16 @@ CREATE TABLE "razao_bloqueio" (
 CREATE TABLE "notificacao" (
     id              SERIAL PRIMARY KEY,
     data            DATE NOT NULL DEFAULT now(),
-    id_razao        INTEGER REFERENCES razao ON DELETE RESTRICT ON UPDATE CASCADE,
+    id_razao        INTEGER REFERENCES razao_bloqueio ON DELETE RESTRICT ON UPDATE CASCADE,
     id_intervencao  INTEGER REFERENCES intervencao ON DELETE CASCADE ON UPDATE CASCADE,
-    TYPE tipo_estado,
-    TYPE tipo_validacao,
-    TYPE tipo_notificacao NOT NULL,
+    estado tipo_estado,
+    validacao tipo_validacao,
+    tipo tipo_notificacao NOT NULL,
 
     CONSTRAINT data_menor_agora CHECK (data <= now()),
-    CONSTRAINT razao_estado_NN  CHECK ((tipo_notificacao='estado_conta' AND id_razao IS NOT NULL AND tipo_estado IS NOT NULL) OR (tipo_notificacao<>'estado_conta' AND id_razao IS NULL AND tipo_estado IS NULL)),
-    CONSTRAINT intervencao_NN   CHECK ((tipo_notificacao<>'estado_conta' AND id_intervencao IS NOT NULL) OR (tipo_notificacao='estado_conta' AND id_intervencao IS NULL)),
-    CONSTRAINT validacao_NN     CHECK ((tipo_notificacao='validacao' AND tipo_validacao IS NOT NULL) OR (tipo_notificacao<>'validacao' AND tipo_validacao IS NULL))
+    CONSTRAINT razao_estado_NN  CHECK ((tipo='estado_conta' AND id_razao IS NOT NULL AND estado IS NOT NULL) OR (tipo<>'estado_conta' AND id_razao IS NULL AND estado IS NULL)),
+    CONSTRAINT intervencao_NN   CHECK ((tipo<>'estado_conta' AND id_intervencao IS NOT NULL) OR (tipo='estado_conta' AND id_intervencao IS NULL)),
+    CONSTRAINT validacao_NN     CHECK ((tipo='validacao' AND validacao IS NOT NULL) OR (tipo<>'validacao' AND validacao IS NULL))
 );
 
 CREATE TABLE "recebe_not" (
