21990ed164bac4dbaec9401874d5bdd0f0e48e09
Patricia Oliveira <up201905427@fe.up.pt>
2021-12-27 20:24:53 +0000

    [CSS] Restructure Admin UC Page layout

--- template-laravel/resources/views/pages/admin/ucs.blade.php
+++ template-laravel/resources/views/pages/admin/ucs.blade.php
@@ -7,43 +7,54 @@
 <section id="admin-ucs-page">
   <h2>Unidades Curriculares</h2> 
 
-  <div class="row"> 
-    <div class="col-12">
-
-      <table id="admin-ucs-table" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
-        <thead>
-          <tr>
-            <th class="th-sm">Nome</th>
-            <th class="th-sm">Sigla</th>
-            <th class="th-sm">Descrição</th>
-            <th class="th-sm">Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($ucs as $uc)
-          <tr>
-            <th scope="row"><a href="{{ url('/ucs/'.$uc->id) }}" class="app-link">{{ $uc->name }}</a></td>
-            <td>{{ $uc->code }}</td>
-            <td>{{ substr($uc->description, 0, 50) }}
-                @if (strlen($uc->description) > 50)
-                ...
-                @endif
-            </td>
-            <!-- mais um com o dropdown de lista dedocentes? -->
-            <td>Editar/eliminar/editarprofessores</td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
-
-      <div class="d-flex">
-        {!! $ucs->links() !!}
-      </div>
-
+  <div class="row my-3">
+    <div class="col-12 admin-action-add">
+      <small class="text-mutted">Gestão de Unidadades Curriculares</small>
+      <a href="{{ url('ucs/create') }}" class="btn btn-primary text-white"><i class="fas fa-plus"></i></a>
     </div>
   </div>
 
-  <!-- bitao nova uc -->
+  <div class="row"> 
+    <div class="col-12">
+      <div class="table-responsive">
+        <table class="table table-striped table-bordered caption-top">
+          <thead>
+            <tr>
+              <th scope="col">Nome</th>
+              <th scope="col">Sigla</th>
+              <th scope="col">Descrição</th>
+              <th scope="col">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($ucs as $uc)
+            <tr data-id="{{ $uc->id }}">
+              <th scope="row"><a href="{{ url('/ucs/'.$uc->id) }}" class="app-link">{{ $uc->name }}</a></td>
+              <td>{{ $uc->code }}</td>
+              <td>
+                {{ substr($uc->description, 0, 50) }}
+                @if (strlen($uc->description) > 50)
+                ...
+                @endif
+              </td>
+              <td class="admin-table-actions">
+                <a href="{{ url('admin/ucs/'.$uc->id.'/teachers') }}" class="btn btn-info text-black me-1"><i class="far fa-address-card"></i></a>
+                <a href="{{ url('ucs/'.$uc->id.'/edit') }}" class="btn btn-warning text-black me-1"><i class="far fa-edit"></i></a>
+                <a href="#" class="btn btn-danger text-white"><i class="far fa-trash-alt"></i></a>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="col-12 d-flex justify-content-end">
+      {!! $ucs->links() !!}
+    </div>
+  </div>
   
 </section>
 
