6e411ece41bf36335b049db3da59b215cdce4def
Henrique Nunes <hrn2001@gmail.com>
2021-12-27 11:16:25 +0000

    User controller and policy

---
+++ template-laravel/app/Policies/UserPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  User  $userAuth
+     * @param  User  $user
+     * @return Response|bool
+     */
+    public function show(User $userAuth, User $user)
+    {
+        return !$userAuth->blocked;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  User  $userAuth
+     * @param  User  $user
+     * @return Response|bool
+     */
+    public function update(User $userAuth, User $user)
+    {
+        return $userAuth->id == $user->id && !$userAuth->blocked;
+    }
+}
