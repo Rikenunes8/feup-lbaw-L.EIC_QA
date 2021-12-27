96297c59c57a65b29fffb19f70584801b91cc097
Henrique Nunes <hrn2001@gmail.com>
2021-12-27 16:49:40 +0000

    [CSS] Page questions with score, date and showing 1 for row

--- template-laravel/resources/views/partials/question.blade.php
+++ template-laravel/resources/views/partials/question.blade.php
@@ -1,14 +1,22 @@
-<div class="col-md-6 col-lg-4 mb-2 px-1">
+<div class="col-md-6 col-lg-4 mb-2 px-1 w-100">
   <div class="card question-card h-100" data-id="{{ $question->id }}">
     <div class="card-body">
-      <h5 class="card-title me-4"><a href="{{ url('/question/'.$question->id) }}">{{ $question->title }}</a></h5>
-      <h6 class="card-subtitle mt-1 mb-2"><span class="badge bg-info text-dark">{{ $question->uc->code }}</span></h6>
-      <p class="card-text">
-        {{ substr($question->text, 0, 100) }}
-        @if (strlen($question->text) > 100)
-        ...
-        @endif
-      </p>
+      <div class="question-votes align-top h-100 d-inline-block ">
+        <span class="badge bg-primary h-100 align-middle">{{ $question->votes }}</span>
+      </div>
+      <div class="h-100 d-inline-block">
+        <h5 class="card-title me-4"><a href="{{ url('/question/'.$question->id) }}">{{ $question->title }}</a></h5>
+        <h6 class="card-subtitle mt-1 mb-2">
+          <span class="badge bg-info text-dark">{{ $question->uc->code }}</span>
+          <span class="">{{ date('d/m/Y H:i', strtotime($question->date)); }}</span>
+        </h6>
+        <p class="card-text">
+          {{ substr($question->text, 0, 100) }}
+          @if (strlen($question->text) > 100)
+          ...
+          @endif
+        </p>
+      </div>
     </div>
   </div>
 </div>
