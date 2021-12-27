f2569d01a69a78f2ced5ce1d7f7cd4bc2cd1a093
Henrique Nunes <hrn2001@gmail.com>
2021-12-27 16:05:25 +0000

    [CSS] Use bootstrap instead of css

--- template-laravel/resources/views/partials/user.blade.php
+++ template-laravel/resources/views/partials/user.blade.php
@@ -1,14 +1,14 @@
 <div class="col-md-6 col-lg-4 mb-2 px-1">
   <div class="card user-card h-100" data-id="{{ $user->id }}">
     <div class="card-body">
-      <a href="#" class="link-dark text-decoration-none users-profile-photo" aria-expanded="false">
+      <a href="#" class="link-dark text-decoration-none d-inline" aria-expanded="false">
         @if ( file_exists( asset('images/users/'.Auth::user()->photo) && Auth::check()) )
         <img src="{{ asset('images/users/'.Auth::user()->photo.'.jpg') }}" alt="profile-photo" id="profile-photo" class="rounded-circle w-32">
         @else
         <img src="{{ asset('images/users/default.jpg') }}" alt="profile-photo" id="profile-photo" class="rounded-circle w-32">
         @endif
       </a>
-      <h5 class="card-title me-4"><a href="{{ url('/users/'.$user->id) }}">{{ $user->name }}</a></h5>
+      <h5 class="card-title me-4 d-inline"><a href="{{ url('/users/'.$user->id) }}">{{ $user->name }}</a></h5>
       <h6 class="card-subtitle mt-1 mb-2"><span class="badge bg-info text-dark">{{ $user->type }}</span>&nbsp;&nbsp;<span class="badge bg-info text-dark">Score: {{ $user->score }}</span></h6>
       <p class="card-text">
         {{ substr($user->about, 0, 100) }}
