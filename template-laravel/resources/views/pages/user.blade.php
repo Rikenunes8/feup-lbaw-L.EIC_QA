57ebb8bd6f12458efadb9fb0adcab62e1bce8468
Henrique Nunes <hrn2001@gmail.com>
2021-12-28 12:10:44 +0000

    Edit user profile

--- template-laravel/resources/views/pages/user.blade.php
+++ template-laravel/resources/views/pages/user.blade.php
@@ -5,14 +5,17 @@
 @section('content')
 
 <section id="user-page">
-  <div class="row user-card" data-id="{{ $user->id }}"> 
+  <div class="row user-profile" data-id="{{ $user->id }}"> 
     <div class="col-12 position-relative">
       @if ( Auth::check() && Auth::user()->id == $user->id )
-        <h2 class="me-4">O meu Perfil</h2> 
+      <div class="float-end">
+        <a href="{{ url('users/'.$user->id.'/edit') }}" class="btn btn-primary text-white"><i class="fas fa-edit"></i></a>
+      </div>
+      <h2 class="me-4">O meu Perfil</h2> 
       @else
-        <h2 class="me-4">Perfil</h2> 
+      <h2 class="me-4">Perfil</h2> 
       @endif
-      <div class="row">
+      <div class="row mt-4">
         <div class="col-md-6 col-lg-4 mb-2 px-1">
           @if ( file_exists( asset('images/users/'.$user->photo) ) )
             <img src="{{ asset('images/users/'.$user->photo.'.jpg') }}" alt="profile-photo" id="profile-photo" class="w-75 h-75">
@@ -24,13 +27,15 @@
           <h3 class="me-4">{{ $user->name }}</h2> 
           <span class="badge bg-info text-dark mt-1 mb-2">{{ $user->type }}</span>
           <span class="mt-1 mb-2">Aderiu a {{ date('d/m/Y', strtotime($user->registry_date)); }}</span>
-          <h4>Sobre mim</h4>
-          <div class="pl-3"> 
+          <h4 class="mt-4">Sobre mim</h4>
+          <div class="px-3"> 
             <p>{{ $user->about }}</p>
-            <p>Aniversário: {{ date('m/d/Y', strtotime($user->birthdate)); }}</p>
+            @if (!is_null($user->birthdate))
+            <p>Aniversário: {{ date('d/m/Y', strtotime($user->birthdate)); }}</p>
+            @endif
           </div>
           <h4>Contactos</h4> 
-          <div class="pl-3">
+          <div class="px-3">
             <p>Email: {{ $user->email }}</p>
           </div>
         </div>
