94edaebf87b9fbe79e842bb4ff6e239b7327632e
Patricia Oliveira <up201905427@fe.up.pt>
2021-12-30 22:18:21 +0000

    [BREADCRUMBS] First implementation of builing the breadcrumbs dynamically

--- template-laravel/resources/views/layouts/breadcrumbs.blade.php
+++ template-laravel/resources/views/layouts/breadcrumbs.blade.php
@@ -1,15 +1,24 @@
-
-@if ($_SERVER['REQUEST_URI'] != '/login')
 <div class="row">
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="#">Home</a></li>
-      <li class="breadcrumb-item active" aria-current="page">Users</li>
+      <li class="breadcrumb-item"><a href="/">Home</a></li>
+
+      <?php $link = "" ?>
+      @for ($i = 1; $i <= count(Request::segments()); $i++)
+          @if ($i < count(Request::segments()) && $i > 0)
+            <?php $link .= "/" . Request::segment($i); ?>
+            <li class="breadcrumb-item">
+              <a href="{{ $link }}"> {{ ucwords(str_replace('-',' ',Request::segment($i)))}} </a>
+            </li>
+          @else 
+            <li class="breadcrumb-item active" aria-current="page">
+              {{ucwords(str_replace('-',' ',Request::segment($i)))}}
+            </li>
+          @endif
+      @endfor
+
     </ol>
   </nav>
 </div>
-@endif
-
-<!-- TODO -->
 
 @yield('breadcrumbs')
