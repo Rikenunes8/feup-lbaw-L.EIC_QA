e110dd18b10efd2dff5789005076b23f6eb5be42
Henrique Nunes <hrn2001@gmail.com>
2022-01-06 19:23:50 +0000

    Generate notifications

--- template-laravel/app/Console/Kernel.php
+++ template-laravel/app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Notifications\NotificationEmail;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +26,20 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        /*$schedule->call(function() {
+            $notificationsToSend = DB::table('receive_not')->where('toEmail', true)->get();
+
+            foreach($notificationsToSend as $not_user) {
+                $not_user->update(['toEmail' => false]);
+                if ($not_user->read) continue;
+                
+                $notification = Notification::find($not_user->id_notification);
+                $user = User::find($not_user->id_user);
+                if ($user->receive_email)
+                    $user->notify(new NotificationEmail($notification));
+            }
+
+        })->everyFiveMinutes();*/
     }
 
     /**
