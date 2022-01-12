b950f4374814e33326cf54d5bb1f1a34ac619d77
Henrique Nunes <hrn2001@gmail.com>
2022-01-12 13:59:03 +0000

    Notifications date in format 'há x tempo'

--- template-laravel/resources/views/partials/notification.blade.php
+++ template-laravel/resources/views/partials/notification.blade.php
@@ -3,7 +3,7 @@
     $read = Auth::user()->notifications()->where('id_notification', $notification->id)->first()->pivot->read;
   @endphp
   <div class="card notification-card {{ $read? 'notification-read':'notification-unread'}} h-100" data-id="{{ $notification->id }}">
-    <div class="card-body">
+    <div class="card-body p-2">
       @php 
         if ($notification->type == 'account_status') {
           $type = 'Novo Estado de Conta';
@@ -24,7 +24,10 @@
       @endphp
       <a href="#" class="app-link" onclick="document.getElementById('redirect-form-{{ $notification->id }}').submit();">
         <h5 class="card-title me-4"> {{ $type }}
-        <span class="text-muted">{{ date('d/m/Y H:i', strtotime($notification->date)); }}</span>
+        @php
+          \Carbon\Carbon::setLocale('pt');
+        @endphp
+        <span class="text-muted">{{ \Carbon\Carbon::createFromFormat('d/m/Y H:i', date('d/m/Y H:i', strtotime($notification->date)))->diffForHumans(); }}</span>
         </h5>
       </a>
       <p class="card-text">
@@ -33,11 +36,11 @@
 
       <div class="text-center notifications-page-actions p-3">
         @if ($read)
-          <a href="#" class="btn btn-outline-dark  text-black notifications-page-envelope me-1"><i class="far fa-envelope"></i></a>
+          <a class="btn btn-outline-dark bg-white text-black notifications-page-envelope me-1"><i class="far fa-envelope"></i></a>
         @else
-          <a href="#" class="btn btn-outline-dark  text-black notifications-page-envelope me-1"><i class="far fa-envelope-open"></i></a>
+          <a class="btn btn-outline-dark bg-white text-black notifications-page-envelope me-1"><i class="far fa-envelope-open"></i></a>
         @endif
-        <a href="#" class="btn btn-outline-dark text-black notifications-page-remove me-1"><i class="far fa-trash-alt"></i></a>
+        <a class="btn btn-outline-dark bg-white text-black notifications-page-remove me-1"><i class="far fa-trash-alt"></i></a>
       </div>
 
       <form method="POST" id="redirect-form-{{ $notification->id }}" class="d-none" 
